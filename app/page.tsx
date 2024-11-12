a90eece8c89ce7f2030464722281c27205e96f6b
nodbew <165175929+nodbew@users.noreply.github.com>
2024-11-12 10:37:09 +0000

    keyboard demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,81 @@
-'use client';
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid2';
 
-import katex from 'katex';
 
-function MathExpression(props: { color?: "red" | "green", children: string }) {
-    // TODO : implement highlighting invalid expressions
-    const html = {
-        __html: katex.renderToString(
-            `\\textcolor{${props.color ?? "black"}}{${props.children}}`
-        ),
-    };
+function Keyboard(props: {
+    layout: Array<Array<string>>,
+    onKeyPress: (key: string) => void,
+}) {
     return (
-        <div dangerouslySetInnerHTML={html} />
-    )
+      <Box
+       sx={{
+         bgcolor: theme => theme.palette.mode === "light" ? "#cccccc": "#6d6d6d",
+         position: 'fixed',
+         height: '50vh',
+         width: '100vw',
+         bottom: 0,
+       }}
+      >
+        <Grid 
+         container
+         direction="column"
+         spacing={2}
+         margin="5%"
+        >
+          {
+            props.layout.map((keys, idx) => (
+              <Grid
+               container
+               spacing={2}
+               size={12}
+               key={`[${keys}]-${idx}`}
+              >
+                {
+                  keys.map((key, idx) => (
+                    <Grid size="grow" key={`${key}-${idx}`} >
+                      <Button
+                       onClick={() => props.onKeyPress(key)}
+                       key={`btn-${key}-${idx}`}
+                       fullWidth
+                       sx={{
+                         color: theme => theme.palette.mode === "light" ? "black" : "white",
+                         bgcolor: theme => theme.palette.mode === "light" ? "white" : "#6c6d6e",
+                         padding: theme => theme.spacing(2),
+                         textAlign: "center",
+                         textTransform: "none",
+                       }}
+                      >
+                        {key}
+                      </Button>
+                    </Grid>
+                  ))
+                }
+              </Grid>
+            ))
+          }
+        </Grid>
+    </Box>
+  );
 }
 
 export default function App() {
+    const [text, setText] = useState('');
+    const layout = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f'],
+        ['h', 'i', 'j'],
+    ];
+    const callback = key => setText(prev => prev + key);
+
     return (
-        <MathExpression>{'\\frac{1}{2}'}</MathExpression>
-    )
+        <div>
+            <p>{text}</p>
+            <Keyboard
+             layout={layout}
+             onKeyPress={callback}
+            />
+        </div>
+    );
 }
