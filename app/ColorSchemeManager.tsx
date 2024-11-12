731d57016960ef1be30041d797a32cc1593efad7
nodbew <165175929+nodbew@users.noreply.github.com>
2024-11-12 22:06:53 +0900

    Create ColorSchemeManager.tsx

---
+++ app/ColorSchemeManager.tsx
@@ -0,0 +1,15 @@
+import { useEffect, Fragment } from 'react';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+
+export default function ColorSchemeManager() {
+  const theme = useTheme();
+  useEffect(
+    () => { 
+      theme.palette.mode = useMediaQuery('(prefers-color-scheme: dark)') ? 'dark' : 'light'; 
+    }, 
+    [useMediaQuery('(prefers-color-scheme: dark)'), theme],
+  );
+  return <Fragment></Fragment>;
+}
