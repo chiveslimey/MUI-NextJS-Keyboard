15068bd12b5b99bffe576ca58a1cb946efe5d93d
nodbew <165175929+nodbew@users.noreply.github.com>
2024-11-13 13:25:14 +0900

    Revert old algorithm

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next";
 import { Noto_Sans_JP } from "next/font/google";
 
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
-import ColorScheme from './ColorScheme';
+import { useMediaQuery } from '@mui/material/useMediaQuery';
 
 import "./globals.css";
 
@@ -24,13 +24,17 @@ export const metadata: Metadata = {
 };
 
 export default function Layout(props: { children: ReactNode }) {
+  const light = createTheme({palette: {mode: 'light'}});
+  const dark = createTheme({palette: {mode: 'dark'}});
+  const theme = useMediaQuery('(prefers-color-scheme: dark)') ? dark : light;
+  
   return (
     <html lang="ja">
       <body className={inter.className}>
         <AppRouterCacheProvider>
-          <ColorScheme>
+          <ThemeProvider theme={theme}>
             {props.children}
-          </ColorScheme>
+          </ThemeProvider>
         </AppRouterCacheProvider>
       </body>
     </html>
