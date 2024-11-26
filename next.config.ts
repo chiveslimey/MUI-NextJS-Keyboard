f750edf9a10f1678152074d45a835dee4f3a904a
nodbew <165175929+nodbew@users.noreply.github.com>
2024-11-26 21:01:57 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  pageExtensions: [
+    "page.tsx",
+    "page.ts",
+    "ts",
+  ],
 };
 
 export default nextConfig;
