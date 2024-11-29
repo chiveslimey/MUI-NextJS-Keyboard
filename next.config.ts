5613dfeb5615b968dddf2b530d65d7817742ecc4
nodbew <165175929+nodbew@users.noreply.github.com>
2024-11-29 09:11:47 +0900

    Restore old version of next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  pageExtensions: [
-    "page.tsx",
-    "page.ts",
-    "ts",
-  ],
 };
 
 export default nextConfig;
