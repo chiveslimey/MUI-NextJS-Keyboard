e92b88c33f59d491b8ddcd4c876d64caf2e91518
nodbew <165175929+nodbew@users.noreply.github.com>
2024-12-01 19:11:44 +0900

    Try separating createTheme from the component

--- app/theme.tsx
+++ app/theme.tsx
@@ -1,29 +1,26 @@
-import { ReactNode, useMemo } from 'react';
+import { ReactNode } from 'react';
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
 import createTheme from '@mui/material/styles/createTheme';
   
+const theme = createTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        lightgray: '#cccccc',
+        invertableWhite: 'white',
+      }
+    },
+    dark: {
+      palette: {
+        lightgray: '#6d6d6d',
+        invertableWhite: '#6c6d6e',
+      }
+    },
+  },
+});
+
 /* Switch between dark and light mode depending on the system default */
 export default function Theme(props: { children: ReactNode }) {
-  // createTheme cannot be mixed with Next.js's SSR
-  const theme = useMemo(
-    () => createTheme({
-      colorSchemes: {
-        light: {
-          palette: {
-            lightgray: '#cccccc',
-            invertableWhite: 'white',
-          }
-        },
-        dark: {
-          palette: {
-            lightgray: '#6d6d6d',
-            invertableWhite: '#6c6d6e',
-          }
-        },
-      },
-    }), 
-    []
-  );
   return (
     <ThemeProvider theme={ theme } >
       { props.children }
